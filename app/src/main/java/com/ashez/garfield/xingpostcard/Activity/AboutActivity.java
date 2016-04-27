3b5e7def8fa9df4e668b4dc7d14d3c3e4e06915a
Garfield <expine@163.com>
2016-04-27 22:09:14 +0800

    1

--- app/src/main/java/com/ashez/garfield/xingpostcard/Activity/AboutActivity.java
+++ app/src/main/java/com/ashez/garfield/xingpostcard/Activity/AboutActivity.java
@@ -14,7 +14,6 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        //KJFDKSLAJLFJAKLSDFJ;
-        //dsadhuashduashu233333333
+        //修改
     }
 }
