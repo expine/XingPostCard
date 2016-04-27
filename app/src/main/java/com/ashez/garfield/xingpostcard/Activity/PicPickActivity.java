83a56a12d92645bf62df1da9640f2ef1af243312
菠萝小莫 <Garfield@Garfield-2.local>
2016-04-27 10:29:12 +0800

    Initial commit

---
+++ app/src/main/java/com/ashez/garfield/xingpostcard/Activity/PicPickActivity.java
@@ -0,0 +1,18 @@
+package com.ashez.garfield.xingpostcard.Activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ashez.garfield.xingpostcard.R;
+
+/**
+ * Created by Garfield on 4/26/16.
+ */
+public class PicPickActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picpick);
+    }
+}
