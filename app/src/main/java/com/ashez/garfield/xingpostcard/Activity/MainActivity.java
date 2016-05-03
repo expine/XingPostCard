9d1a9cd882a609ac9b85b8e2d700ae40b12f9f72
li853281682 <853281682@qq.com>
2016-05-03 22:02:28 +0800

    di第一第二个界面

--- app/src/main/java/com/ashez/garfield/xingpostcard/Activity/MainActivity.java
+++ app/src/main/java/com/ashez/garfield/xingpostcard/Activity/MainActivity.java
@@ -1,15 +1,28 @@
 package com.ashez.garfield.xingpostcard.Activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.ashez.garfield.xingpostcard.R;
+import com.ashez.xingpostcard.adapter.CoverFlow;
+import com.ashez.xingpostcard.adapter.CoverFlowSampleAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
+    private CoverFlow fancyCoverFlow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.fancyCoverFlow = (CoverFlow) this.findViewById(R.id.fancyCoverFlow);
+
+        this.fancyCoverFlow.setAdapter(new CoverFlowSampleAdapter());
+        this.fancyCoverFlow.setUnselectedAlpha(1.0f);
+        this.fancyCoverFlow.setUnselectedSaturation(0.0f);
+        this.fancyCoverFlow.setUnselectedScale(0.5f);
+        this.fancyCoverFlow.setSpacing(50);
+        this.fancyCoverFlow.setMaxRotation(0);
+        this.fancyCoverFlow.setScaleDownGravity(0.2f);
+        this.fancyCoverFlow.setActionDistance(CoverFlow.ACTION_DISTANCE_AUTO);
     }
 }
