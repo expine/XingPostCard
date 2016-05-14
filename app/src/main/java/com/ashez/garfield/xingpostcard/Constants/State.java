4704858aa773a0275746dcb9bef6872a184e2dd7
Garfield <expine@163.com>
2016-05-15 02:12:07 +0800

    :)更新

---
+++ app/src/main/java/com/ashez/garfield/xingpostcard/Constants/State.java
@@ -0,0 +1,14 @@
+package com.ashez.garfield.xingpostcard.Constants;
+
+/**
+ * Created by Garfield on 5/15/16.
+ *
+ * 状态记录类
+ */
+public class State {
+    public static int myImageViewPhoto = 0;//用于记录照片的R.id
+    public static String myImageViewPhotoURL = "";//用于记录照片的URL
+
+    public static boolean hasPhoto = false;//用于记录image中是否有照片
+
+}
