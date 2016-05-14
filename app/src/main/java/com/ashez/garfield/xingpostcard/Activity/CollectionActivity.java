49f15682b09afca85f8b9e4bc7018256254d3a7c
li853281682 <853281682@qq.com>
2016-05-15 02:13:49 +0800

    点击传值

--- app/src/main/java/com/ashez/garfield/xingpostcard/Activity/CollectionActivity.java
+++ app/src/main/java/com/ashez/garfield/xingpostcard/Activity/CollectionActivity.java
@@ -1,24 +1,19 @@
 package com.ashez.garfield.xingpostcard.Activity;
 
-import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.ashez.garfield.xingpostcard.R;
 import com.ashez.xingpostcard.adapter.Collection_SimpleAdapter;
 
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -31,6 +26,7 @@ public class CollectionActivity extends AppCompatActivity {
     public Collection_SimpleAdapter mAdapter_real,mAdapter_cartoon;
     Context con;
     Button bendi;
+    int Pick_position;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -39,32 +35,70 @@ public class CollectionActivity extends AppCompatActivity {
         initData();
         initView();
         // 第一个参数是key， 第二个参数是若给定key对应的值不存在则默认的返回值；
-        int Pick_position = getIntent().getIntExtra("Pick_position",-1);
-        System.out.println("Pick_position:"+Pick_position);
+         Pick_position = getIntent().getIntExtra("styleCode",-1);
+        System.out.println("styleCode:"+Pick_position);
 //        img.setImageResource(mData.get(id));
 
         view_layout(); //布局设置
         select_Click();//点击选择本地图片
 //        get_picture();
+        real_Click();
+        cartoon_Click();
 
 
+//"pictureFromApp" 卡通过真实
 
 
     }
+
+    private void cartoon_Click() {
+        mAdapter_cartoon.setOnItemClickListener(new Collection_SimpleAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Intent intent = new Intent(CollectionActivity.this, ArtActivity.class);
+                //  第一个参数是key， 第二个参数是要传递的值
+                intent.putExtra("pictureFromApp",
+                        Collection_data_cartoon.get(position));
+                intent.putExtra("styleCode",Pick_position);
+                CollectionActivity.this.startActivity(intent);
+            }
+        });
+
+    }
+
+    private void real_Click() {
+        mAdapter_real.setOnItemClickListener(new Collection_SimpleAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Intent intent = new Intent(CollectionActivity.this, ArtActivity.class);
+                //  第一个参数是key， 第二个参数是要传递的值
+                intent.putExtra("pictureFromApp",
+                        Collection_data_real.get(position));
+                intent.putExtra("styleCode",Pick_position);
+                CollectionActivity.this.startActivity(intent);
+            }
+        });
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == RESULT_OK) {
-            Uri uri = data.getData();
-            Log.e("uri", uri.toString());
-            ContentResolver cr = this.getContentResolver();
-            try {
-                Bitmap bitmap = BitmapFactory.decodeStream(cr.openInputStream(uri));
+            Uri uri = data.getData();//"pictureFromLocal"
+            Intent intent=new Intent(CollectionActivity.this,ArtActivity.class);
+            intent.putExtra("pictureFromLocal",
+                    uri);
+            intent.putExtra("styleCode",Pick_position);
+            CollectionActivity.this.startActivity(intent);
+
+
+
+
+//            ContentResolver cr = this.getContentResolver();
+            //   Bitmap bitmap = BitmapFactory.decodeStream(cr.openInputStream(uri));
+            System.out.println("vvvvvvvvvvvv"+uri);
 //                img = (ImageView) findViewById(R.id.img);
                 /* 将Bitmap设定到ImageView */
 //                img.setImageBitmap(bitmap);  //获取图片
-            } catch (FileNotFoundException e) {
-                Log.e("Exception", e.getMessage(),e);
-            }
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
